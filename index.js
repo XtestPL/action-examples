b49f1f4403819408e5e4ef89d4ffd8159debff7c
Dariusz Szyszlak <dszyszlak@parasoft.com>
2020-12-02 12:48:42 +0100

    Run async

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-try {
-    const buildSystem = core.getInput('build-system');
-    console.log(`Jtest tool is going to use ${buildSystem}!`);
-    if (buildSystem == 'maven') {
-        await exec.exec('mvn jtest:jtest');
-    } else if (buildSystem == 'gradle') {
-        await exec.exec('gradlew clean assemble jtest');
+async function run() {
+    try {
+        const buildSystem = core.getInput('build-system');
+        console.log(`Jtest tool is going to use ${buildSystem}!`);
+        if (buildSystem == 'maven') {
+            await exec.exec('mvn jtest:jtest');
+        } else if (buildSystem == 'gradle') {
+            await exec.exec('gradlew clean assemble jtest');
+        }
+    } catch (error) {
+        core.setFailed(error.message);
     }
-} catch (error) {
-    core.setFailed(error.message);
 }
+
+run();
