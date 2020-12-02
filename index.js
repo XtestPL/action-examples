d57869b437e8a3eb34d0caa155d03afe4326f721
Dariusz Szyszlak <dszyszlak@parasoft.com>
2020-12-02 13:06:34 +0100

    Upgrade run script

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const exec = require('@actions/exec');
 async function run() {
     try {
         const buildSystem = core.getInput('build-system');
+        const buildSystemArgs = core.getInput('build-system-args');
         console.log(`Jtest tool is going to use ${buildSystem}!`);
         if (buildSystem == 'maven') {
-            await exec.exec('mvn jtest:jtest');
+            await exec.exec('mvn jtest:jtest' + ' ' + buildSystemArgs);
         } else if (buildSystem == 'gradle') {
-            await exec.exec('gradlew clean assemble jtest');
+            await exec.exec('gradlew clean assemble jtest' + ' ' + buildSystemArgs);
         }
     } catch (error) {
         core.setFailed(error.message);
